7129fde9091242c8300aa51e514e33b4596272bf
MorpheusVibez <halodre24@gmail.com>
2020-05-25 15:45:05 -0400

    Added signup route/erb file for Students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,27 @@
 class StudentsController < ApplicationController
 
+    get '/signup' do
+        if !logged_in?
+            erb :'students/signup'
+        else
+            redirect to '/courses' 
+        end
+    end
+    
     get '/login' do
         if !logged_in?
             erb :'/students/login'
         else
             redirect to '/courses'
+        end
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.destroy
+            redirect to "/login" 
+        else
+            redirect to "/"
         end
     end
 
