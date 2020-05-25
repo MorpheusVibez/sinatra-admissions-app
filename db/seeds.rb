465fe0a756aab491d16a97bffc4ce38c05fdae97
MorpheusVibez <halodre24@gmail.com>
2020-05-25 15:19:14 -0400

    Updated Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@
 andre = Student.create(name: "Andre", email: "andre@andre.com", password: "password12")
 aisha = Student.create(name: "Aisha", email: "aisha@aisha.com", password: "password23")
 
-course_1 = Course.create(name: "Software Engineering", description: "Learn how to code!", user_id: andre.id)
-course_2 = Course.create(name:"Biology", description: "Learn about Biology!", user_id: aisha.id)
+course_1 = Course.create(name: "Software Engineering", description: "Learn how to code!")
+course_2 = Course.create(name:"Biology", description: "Learn about Biology!")
 
