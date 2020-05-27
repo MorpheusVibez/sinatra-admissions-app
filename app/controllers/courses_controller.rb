8d64d880546a2de289dba3200e8ec976e5540ff4
MorpheusVibez <halodre24@gmail.com>
2020-05-26 21:15:19 -0400

    built courses controller with RESTFUL routes

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,30 @@
+class CoursesController < ApplicationController
+
+    get '/courses' do
+        
+    end
+
+    get '/courses/new' do
+        
+    end
+
+    post '/courses' do
+        
+    end
+
+    get '/courses/:id' do
+        
+    end
+
+    get '/courses/:id/edit' do
+        
+    end
+
+    patch '/courses/:id' do
+        
+    end
+
+    delete '/courses/:id/delete' do
+        
+    end
+end
